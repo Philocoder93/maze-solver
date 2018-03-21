d9033655811c2a16638e63f48170fa24f3ab9781
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2018-03-20 21:28:57 -0400

    cleaned up this code, removed some hard coded values, fixed some errors in the comments

--- solver.scm
+++ solver.scm
@@ -16,10 +16,11 @@
         ; here we check to see if the latest member of start matches end, if so then this branch is done
         (finished? (and (= start-x end-x) (= start-y end-y )))
         ; here we check to see if we've gone out of bounds, if so this branch is done
-        (out-of-bounds? (lambda (x) (or (< x 0) (> x 4))))
+        ; the reason that we use not less than here is to reproduce the behavior of a >=
+        (out-of-bounds? (lambda (x) (or (< x 0) (not (< x side-length)))))
         (left-board? (or (out-of-bounds? start-x) (out-of-bounds? start-y)))
-        ; here we check to see if we've gone out of bounds, if we have then we're done, this is because if we allowed out
-        ; of bounds then you end up with branches that endlessly toggle between two squares
+        ; here we check to see if we've taken a step back, if we have then we're done, this is because if you allow
+        ; steps back then you end up with branches that endlessly toggle between two squares
         (step-back? (and (> (length start) 2) (and (= start-x (car (third start))) (= start-y (cdr (third start))))))
         ; here we check to see if we've hit a wall, if we have then this branch is done
         (hit-wall?  (and (not left-board?) (= 1 (list-ref (list-ref maze start-x) start-y))))
@@ -37,29 +38,29 @@
       (else (let* (
                   ; here we generate the move histories of the four possible moves that
                   ; could be made from this point
-                  (up-one (cons (cons start-x (+ 1 start-y)) start))
-                  (right-one (cons (cons (+ 1 start-x) start-y) start))
-                  (left-one (cons (cons (- start-x 1) start-y) start))
-                  (down-one (cons (cons start-x (- start-y 1)) start))
+                  (up (cons (cons start-x (+ 1 start-y)) start))
+                  (right (cons (cons (+ 1 start-x) start-y) start))
+                  (left (cons (cons (- start-x 1) start-y) start))
+                  (down (cons (cons start-x (- start-y 1)) start))
                   ; and call them
-                  (up-worked? (maze-solver up-one end maze))
-                  (right-worked? (maze-solver right-one end maze))
-                  (down-worked? (maze-solver down-one end maze))
-                  (left-worked? (maze-solver left-one end maze))
+                  (up-move (maze-solver up end maze))
+                  (right-move (maze-solver right end maze))
+                  (down-move (maze-solver down end maze))
+                  (left-move (maze-solver left end maze))
                   )
               (cond
                 ; if any of those branches worked then their first element will be a true boolean
                 ; and they will be the return value from this node
                 ; otherwise we simply return the left call
-                ((car up-worked?) up-worked?)
-                ((car right-worked?) right-worked?)
-                ((car down-worked?) down-worked?)
-                (else left-worked?)))))))
+                ((car up-move) up-move)
+                ((car right-move) right-move)
+                ((car down-move) down-move)
+                (else left-move)))))))
                 
 (define starter (list (cons 4 0)))
 
-(define ender (cons 0 0))
+(define ender (cons 0 4))
 
-(define test-maze (list (list 0 0 0 0 0)(list 0 1 1 1 0)(list 0 1 1 1 0)(list 0 1 1 1 0)(list 0 0 0 0 0)))
+(define test-maze (list (list 0 0 0 0 0)(list 0 1 1 1 1)(list 0 0 0 0 0)(list 1 1 1 1 0)(list 0 0 0 0 0)))
 
 (maze-solver starter ender test-maze)
